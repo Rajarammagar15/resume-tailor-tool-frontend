e5699080c802ed930a205beced26d7d0cac136d7
rajarammagar15 <rajarammagar15@gmail.com>
2026-03-11 10:25:24 +0530

    feedback link updated

--- src/components/FeedbackButton.jsx
+++ src/components/FeedbackButton.jsx
@@ -4,7 +4,8 @@ function FeedbackButton() {
 
   const openFeedback = () => {
     window.open(
-      "https://ats-resume-ai-tool.canny.io/",
+    //   "https://ats-resume-ai-tool.canny.io/",
+    "https://docs.google.com/forms/d/e/1FAIpQLSeZRUewL_j9DDdP8gplOUKfY0igrZwxQGjVu6G2oRQ70EP16Q/viewform?usp=publish-editor",
       "_blank"
     );
   };
